0d7aa99586c3261a7ec78c326508390b90874ec7
Dmitry <DDSurok@gmail.com>
2023-12-19 23:37:03 +0300

    Fix minimal

--- py_debug/util.py
+++ py_debug/util.py
@@ -11,9 +11,8 @@ def log_running_time(func, level: int = logging.DEBUG):
     def wrapper(*args, **kwargs):
         start = datetime.now()
         res = func(*args, **kwargs)
-        if level in (logging.getLevelNamesMapping().values()):
-            log_method = getattr(logging, logging.getLevelName(level))
-            log_method(f'The call [{func.__name__}] is completed in {(datetime.now()-start).total_seconds()}.')
+        if not logging.getLevelName(level).startswith('Level '):
+            logging.log(level, f'The call [{func.__name__}] is completed in {(datetime.now()-start).total_seconds()}.')
         else:
             logging.warning('Log level has not found.')
         return res
@@ -23,9 +22,8 @@ def log_running_time(func, level: int = logging.DEBUG):
 def log_args(func, level: int = logging.DEBUG):
     @functools.wraps(func)
     def wrapper(*args, **kwargs):
-        if level in (logging.getLevelNamesMapping().values()):
-            log_method = getattr(logging, logging.getLevelName(level))
-            log_method(f'Call {func.__name__} with args: [{args}] and kwargs: [{kwargs}].')
+        if not logging.getLevelName(level).startswith('Level '):
+            logging.log(level, f'Call {func.__name__} with {args = } and {kwargs = }.')
         else:
             logging.warning('Log level has not found.')
         res = func(*args, **kwargs)
@@ -33,18 +31,18 @@ def log_args(func, level: int = logging.DEBUG):
     return wrapper
 
 
-def call_counter(func, level: int = logging.DEBUG):
+def call_counter(func, level: int = logging.DEBUG, mute_after: int = 5, log_every: int = 10):
     @functools.wraps(func)
     def wrapper(*args, **kwargs):
-        if func.__name__ in __call_counters.keys:
+        if func.__name__ in __call_counters:
             __call_counters[func.__name__] += 1
         else:
             __call_counters[func.__name__] = 1
-        if level in (logging.getLevelNamesMapping().values()):
-            log_method = getattr(logging, logging.getLevelName(level))
-            log_method(f'Function {func.__name__} has been called {__call_counters[func.__name__]} time(s).')
-        else:
-            logging.warning('Log level has not found.')
+        if __call_counters[func.__name__] - 1 in range(mute_after) or not __call_counters[func.__name__] % log_every:
+            if not logging.getLevelName(level).startswith('Level '):
+                logging.log(level, f'Function {func.__name__} has been called {__call_counters[func.__name__]} times.')
+            else:
+                (logging.warning('Log level has not found.'))
         res = func(*args, **kwargs)
         return res
 
