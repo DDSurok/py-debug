fc1070342992aad88c38d6c5ff9142b6ad0196fa
Dmitry <DDSurok@gmail.com>
2023-12-21 22:40:56 +0300

    Fix minimal

--- tests/test_call_function.py
+++ tests/test_call_function.py
@@ -12,13 +12,10 @@ logging.basicConfig(level=logging.DEBUG)
 
 
 def test_call_debug():
-    try:
-        for a in range(1, 100, 5):
-            for b in range(1, -100, -10):
-                func_add(a, b)
-        assert True
-    except:
-        assert False
+    for a in range(1, 100, 5):
+        for b in range(1, -100, -10):
+            func_add(a, b)
+    assert True
 
 
 if __name__ == '__main__':
